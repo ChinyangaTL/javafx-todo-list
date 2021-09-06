a7dc2f28c8a5ca2c1e09eae6d5b6e11e3820cced
Les Chinyanga <cse20-030@thuto.bac.ac.bw>
2021-09-06 18:37:56 +0200

    Implemented delete todo item

--- src/com/github/chinyangatl/todolist/Controller.java
+++ src/com/github/chinyangatl/todolist/Controller.java
@@ -4,6 +4,8 @@ import com.github.chinyangatl.todolist.datamodel.TodoData;
 import com.github.chinyangatl.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -30,6 +32,8 @@ public class Controller {
     private Label labelDeadline;
     @FXML
     private BorderPane mainBorderPane;
+    @FXML
+    private ContextMenu listContextMenu;
 
     public void initialize() {
 //        TodoItem item1 = new TodoItem("Quit Java", "Give up on this terrible language",
@@ -48,6 +52,16 @@ public class Controller {
 //        todoItems.add(item4);
 //
 //        TodoData.getInstance().setTodoItems(todoItems);
+        listContextMenu = new ContextMenu();
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        deleteMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                deleteItem(item);
+            }
+        });
+        listContextMenu.getItems().addAll(deleteMenuItem);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -89,6 +103,15 @@ public class Controller {
                         }
                     }
                 };
+
+                cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
+                    if(isNowEmpty) {
+                        cell.setContextMenu(null);
+                    }
+                    else {
+                        cell.setContextMenu(listContextMenu);
+                    }
+                });
 
                 return cell;
             }
@@ -132,5 +155,16 @@ public class Controller {
         labelDeadline.setText(item.getDeadline().toString());
 
 
+    }
+
+    public void deleteItem(TodoItem item) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete To Do Item");
+        alert.setHeaderText("Delete item: " + item.getShortDescription());
+        alert.setContentText("Are you sure, OK to confirm");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            TodoData.getInstance().deleteTodoItem(item);
+        }
     }
 }
--- src/com/github/chinyangatl/todolist/datamodel/TodoData.java
+++ src/com/github/chinyangatl/todolist/datamodel/TodoData.java
@@ -94,4 +94,8 @@ public class TodoData {
         todoItems.add(item);
     }
 
+    public void deleteTodoItem(TodoItem item) {
+        todoItems.remove(item);
+    }
+
 }
